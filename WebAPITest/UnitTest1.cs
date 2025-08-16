1222a3f72cc7bfd4c9b088182fd2e2a3c1088578
Shantanu <7shantanusharma@gmail.com>
2025-08-16 19:52:18 +0530

    updated test cases

--- WebAPITest/UnitTest1.cs
+++ WebAPITest/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace WebAPITest
         [Fact]
         public void Add_ShouldReturnSum()
         {
-            Assert.Equal(4, 2 + 2);
+            Assert.Equal(5, 2 + 3);
         }
 
         [Theory]
