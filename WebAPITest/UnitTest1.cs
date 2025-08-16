b5db6a56e4b77c6c65948e40713bcdabb3ba3f23
Shantanu <7shantanusharma@gmail.com>
2025-08-16 19:17:13 +0530

    Added test project

---
+++ WebAPITest/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace WebAPITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_ShouldReturnSum()
+        {
+            Assert.Equal(5, 2 + 2);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(10, 5, 15)]
+        public void Add_TheoryTests(int a, int b, int expected)
+        {
+            Assert.Equal(expected, a + b);
+        }
+    }
+}
